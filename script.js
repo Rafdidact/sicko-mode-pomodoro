9b2e29396d7732ce621d8f1447250d0d077f3334
Rafa ✏️ <77062385+Rafdidact@users.noreply.github.com>
2021-03-17 21:58:34 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+const startingTime = .2;
+let time = startingTime * 60;
+
+const countdownTitle = document.getElementById("countdownTitle");
+const countdownBtn = document.getElementById("countdown");
+const song = document.getElementById("song");
+
+countdownBtn.onclick = function() {
+    startCountdown;
+    setInterval(startCountdown, 1000);
+}
+
+function startCountdown() {
+    const minutes = Math.floor(time / 60);
+    let seconds = time % 60;
+    if (seconds < 10) {
+        seconds = "0" + seconds;
+    }
+    time--;
+    countdownTitle.innerText = `${minutes}:${seconds}`;
+    if (time < 0) {
+        countdownTitle.innerText = "done!";
+        song.play();
+    }
+}
