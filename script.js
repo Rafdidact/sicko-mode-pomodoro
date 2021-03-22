369ae0e515c2737f59e3eae0e3a8ebd2edd3f313
Rafa ✏️ <adanescollante@gmail.com>
2021-03-22 00:51:04 -0400

    updated JavaScript

--- script.js
+++ script.js
@@ -1,30 +1,29 @@
-let time = 25 * 60;
-
 const countdownTitle = document.getElementById("countdownDisplay");
 const countdownBtn = document.getElementById("countdownBtn");
 const song = document.getElementById("song");
 
-countdownBtn.onclick = function() {
-    if (time < 0) {
-        window.alert("Please set the time!");
-    } else if (time != 1, 2, 3, 4, 5, 6, 7, 8, 9) {
-        window.alert("Please ");
-    } else {
-        startCountdown;
-        setInterval(startCountdown, 1000);
-    }
-}
+let time = 25 * 60;
+let minutes;
+let seconds;
+let itrvl;
 
-function startCountdown() {
-    const minutes = Math.floor(time / 60);
-    let seconds = time % 60;
-    if (seconds < 10) {
-        seconds = "0" + seconds;
+window.addEventListener("click", (e) => {
+    const target = e.target.textContent;
+    if (target === "start 25min session") {
+        itrvl = setInterval(timeCalc, 1000);
+    } else if (target === "restart") {
+        clearInterval(itrvl);
+        countdownBtn.textContent = "start 25min session";
+    } else {
+        return;
     }
+})
+
+function timeCalc() {
+    minutes = Math.floor(time / 60);
+    seconds = time % 60;
+    seconds = seconds < 10 ? "0" + seconds : seconds;
+    countdownTitle.textContent = `${minutes}:${seconds}`;
+    countdownBtn.textContent = "restart";
     time--;
-    countdownTitle.innerText = `${minutes}:${seconds}`;
-    if (time < 0) {
-        countdownTitle.innerText = "done!";
-        song.play();
-    }
 }
