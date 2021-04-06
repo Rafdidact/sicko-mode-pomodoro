d90418f484090ca8bedf495cf391014e33244d6b
Rafa <adanescollante@gmail.com>
2021-04-06 11:08:31 -0400

    Revert "Added new set your own time option with HTML, CSS and JavaScript"
    This reverts commit 537acc49c8b945de3a1968fb27b647f68dcf817f.

--- script.js
+++ script.js
@@ -1,18 +1,13 @@
 const countdownTitle = document.getElementById("countdownDisplay");
-const countdownBtn25min = document.getElementById("countdownBtn25min");
-const countdownBtnCustom = document.getElementById("countdownBtnCustom");
-const inputTime = document.getElementById("inputTime");
-const setTime = document.getElementById("setTime");
+const countdownBtn = document.getElementById("countdownBtn");
 const song = document.getElementById("song");
 
-let startingTime = 25;
-let time = startingTime * 60;
+let time = 25 * 60;
 let minutes;
 let seconds;
 let itrvl;
 
 window.addEventListener("click", (e) => {
-    e.preventDefault();
     const target = e.target.textContent;
     if (target === "start 25min session" || target === "continue") {
         itrvl = setInterval(timeCalc, 1000);
@@ -20,16 +15,7 @@ window.addEventListener("click", (e) => {
         song.currentTime = 0;
     } else if (target === "pause") {
         clearInterval(itrvl);
-        countdownBtn25min.textContent = "continue";
-    } else if (target === "Go!") {
-        startingTime = inputTime.value;
-        inputTime.value = "";
-        if (startingTime >= 25 || startingTime !== Number) {
-            window.alert("Please set a time below 25min!");
-            return;
-        } else {
-            itrvl = setInterval(timeCalc, 1000);
-        }
+        countdownBtn.textContent = "continue";
     } else {
         return;
     }
@@ -40,13 +26,13 @@ function timeCalc() {
     seconds = time % 60;
     seconds = seconds < 10 ? "0" + seconds : seconds;
     countdownTitle.textContent = `${minutes}:${seconds}`;
-    countdownBtn25min.textContent = "pause";
+    countdownBtn.textContent = "pause";
     time--;
     if (time < 0) {
         song.play();
         clearInterval(itrvl);
         countdownTitle.textContent = "you're done!";
-        countdownBtn25min.textContent = "start 25min session";
+        countdownBtn.textContent = "start 25min session";
         time = 25 * 60;
     } else {
         return;
